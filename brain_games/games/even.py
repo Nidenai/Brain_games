3a43693d94348752eb87278152640b53839de8ce
Dmitriy Knyazev <dimsknz@gmail.com>
2021-05-09 22:56:21 +0700

    check one.three step

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,5 +1,5 @@
 from random import randint
-TEXT_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
+
 
 def even():
     question = randint(1, 100)
@@ -10,4 +10,3 @@ def even():
     return (question, real_answer)
 
 
-print(even())
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from brain_games.games.even import even
 from brain_games.scripts.brain_engine import brain_play
-from brain_games.games.even import TEXT_TASK
+
 
 def main():
     brain_play(even())
