b10e195f944a5cf7893bd56ee8b8eb4edbb6bf62
Dmitriy Knyazev <dimsknz@gmail.com>
2021-05-11 13:07:00 +0700

    patch 1.0.3 (bug fix: исправлено отображение арифметической прогрессии для пользователя)

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -17,9 +17,11 @@ def function():
     rand_progress = ar_progress(first_num, list_step, list_lenght)
     random_element = randint(0, (len(rand_progress)-1))
     real_answer = rand_progress[random_element]
-    question = rand_progress
-    question.pop(random_element)
-    question.insert(random_element, "..")
+    list_middle = rand_progress
+    list_middle.pop(random_element)
+    list_middle.insert(random_element, "..")
+    question = ' '.join(map(str, list_middle))
     return (question, real_answer)
 
 TASK_TEXT = 'What number is missing in the progression?'
+
