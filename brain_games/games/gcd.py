bcfc812b4985d585b5f04721c86f7e54c4ab5c5d
Dmitriy Knyazev <dimsknz@gmail.com>
2021-05-11 08:18:45 +0700

    patch 1.0.1 (bug fix: исправлена постановка вопроса в игре с делителем)

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,7 +4,7 @@ from math import gcd
 def function():
     random_num_one = randint(1, 100)
     randon_num_two = randint(1, 100)
-    question = str(random_num_one) + ', ' + str(randon_num_two)
+    question = str(random_num_one) + ' ' + str(randon_num_two)
     real_answer = gcd(random_num_one, randon_num_two)
     return (question,real_answer)
 
