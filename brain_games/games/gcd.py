59983c413ebc904e08699302ea8688a86cbbdb8b
Dmitriy Knyazev <dimsknz@gmail.com>
2021-05-17 23:07:25 +0700

    patch 1.0.4 (bug fix: some issues)

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,11 +1,13 @@
 from random import randint
 from math import gcd
 
+
 def function():
     random_num_one = randint(1, 100)
     randon_num_two = randint(1, 100)
     question = str(random_num_one) + ' ' + str(randon_num_two)
     real_answer = gcd(random_num_one, randon_num_two)
     return (question,real_answer)
+
 
 TASK_TEXT = 'Find the greatest common divisor of given numbers'
